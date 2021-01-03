6d8ebdcc5af04d6c086baa3481de952cd69449e5
GrzegorzNecka <strzypior@gmail.com>
2021-01-03 18:27:18 +0100

    2 files modified

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# oj-winda
+# Przeprogramowani.pl - Opanuj JavaScript
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/oj-winda)
+Moduł II. - Przeglądarka bez tajemnic
+
+## Ćwiczenie nr. 4 - Winda
+
+### Jak zacząć
+
+Kliknij przycisk "Fork" u góry edytora aby skopiować ćwiczenie.
+ 
+### Cel ćwiczenia
+
+Winda napędzana scrollem użytkownika porusza się w górę i w dół (zgodnie z kierunkiem scrollowania). 
+
+Spraw, aby pole nad windą, informujące o kierunku poruszania się, było aktualizowane na bieżąco.
+
+### Punkty bonusowe
+
+1. Spraw, aby na polu z informacjami pokazywało się również aktualne piętro
+
+### podpowiedzi
+1. https://developer.mozilla.org/pl/docs/Web/API/Window/requestAnimationFrame
+2. https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-import './style.css';
+import "./style.css";
 
 const directions = {
-  top: '🔼',
-  bottom: '🔽'
+  top: "🔼",
+  bottom: "🔽"
+};
+
+let currentScrollPos = 0;
+
+// DOM elemenst
+const directionElem = document.querySelector(".direction");
+const elevator = document.querySelector(".elevator");
+
+function setDirection(prevScrollPos, currentScrollPos) {
+  if (prevScrollPos < currentScrollPos) {
+    directionElem.innerText = `Kierunek: ${directions.bottom}`;
+  } else if (prevScrollPos > currentScrollPos) {
+    directionElem.innerText = `Kierunek: ${directions.top}`;
+  }
 }
+
+function setTimePeriod(prevScrollPos, currentScrollPos) {
+  setTimeout(function() {
+    setDirection(prevScrollPos, currentScrollPos);
+  }, 200);
+}
+
+elevator.addEventListener("scroll", e => {
+  let previewScrollPos = currentScrollPos;
+  currentScrollPos = Math.round(e.target.scrollTop);
+
+  setTimePeriod(previewScrollPos, currentScrollPos);
+});
